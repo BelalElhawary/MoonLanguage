cf5c3e521f399836cbd3df2ab65ba1d7756b9f5b
Belal Mohamed <73793768+BelalElhawary@users.noreply.github.com>
2021-12-08 19:48:06 +0200

    0.2

--- shell.py
+++ shell.py
@@ -2,6 +2,6 @@ import MoonLanguage
 
 while True:
     text = input('ml >')
-    result, error = MoonLanguage.run('<stdin>', text)
+    result, error = MoonLanguage.run('test', text)
     if error: print(error.as_string())
     else: print(result)
